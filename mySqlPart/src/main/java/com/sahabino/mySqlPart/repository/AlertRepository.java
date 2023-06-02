c2490f087be8836f9809a41c88d7707bab40527b
AliBazoubandi <72089608+AliBazoubandi@users.noreply.github.com>
2023-06-02 12:35:53 +0330

    delete mySqlPart directory
    i want to have every class and component on a main directory so i delete this

--- mySqlPart/src/main/java/com/sahabino/mySqlPart/repository/AlertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sahabino.mySqlPart.repository;
-
-import com.sahabino.mySqlPart.api.model.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepository extends JpaRepository<Alert, Integer> {
-}
